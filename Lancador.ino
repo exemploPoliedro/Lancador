540a991f7326697b7e80dfb8643510470eb9e076
Davi Grossi Hasuda <davigrossihasuda@MacBook-Pro-de-Davi.local>
2018-08-04 20:45:57 -0300

    Prototipos das funcoes adicionados

--- Lancador.ino
+++ Lancador.ino
@@ -1,3 +1,18 @@
+#define LED_1_PIN 2
+#define LED_2_PIN 5
+#define LED_3_PIN 7
+
+#define MOTOR_IN_1_PIN 24
+#define MOTOR_IN_2_PIN 26
+
+#define BUTTON_PIN 9
+
+// Função do motor
+void motorLancar();
+
+// Função do LED
+void 
+
 void setup() {
   // put your setup code here, to run once:
 
