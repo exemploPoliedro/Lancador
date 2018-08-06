0f13da7000286c69b56e97dac71fd0eceecf11a3
exemploPoliedro <41811152+exemploPoliedro@users.noreply.github.com>
2018-08-06 18:32:12 -0300

    Update leds.ino
    TUDO

--- Lancador/leds.ino
+++ Lancador/leds.ino
@@ -1,8 +1,19 @@
 void contarRegressivamente() {
-
+  
   // Colocar aqui o código dos LEDs
   // Ascender os LEDs de forma a ser uma contagem regressiva
   // Os pinos dos LEDs são LED_1_PIN, LED_2_PIN e LED_3_PIN
+  
+  digitalWrite(LED_1_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_2_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_3_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_1_PIN, LOW);
+  digitalWrite(LED_2_PIN, LOw);
+  digitalWrite(LED_3_PIN, LOW);
+  
   
 }
 
