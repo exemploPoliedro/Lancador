649b2504169698d956ca4b0cf6eaa1a00435d915
exemploPoliedro <41811152+exemploPoliedro@users.noreply.github.com>
2018-08-06 18:27:28 -0300

    Update motor.ino
    Primeira versão do motor (julgando que é tipo uma catapulta).

--- Lancador/motor.ino
+++ Lancador/motor.ino
@@ -2,8 +2,12 @@ void motorLancar() {
 
   // Colocar aqui o código que ativa o motor e lança o objeto
   // os pinos do motor são MOTOR_IN_1_PIN e MOTOR_IN_2_PIN
-
-
+  
+  digitalWrite(MOTOR_IN_1_PIN, LOW);
+  digitakWrite(MOTOR_IN_2_PIN, HIGH);
+  delay(250);
+  digitalWrite(MOTOR_IN_1_PIN, HIGH);
+  digitakWrite(MOTOR_IN_2_PIN, HIGH);
   
 }
 
